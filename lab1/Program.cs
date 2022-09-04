f4f4c382ac10a128af3c9fc0b9583d94ec93357c
Andrey525 <ghost5run@yandex.ru>
2022-09-04 14:06:08 +0300

    Add 3 tasks from 4

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,4 +1,5 @@
-﻿class Program
+﻿
+class Program
 {
     static void PrintMenu()
     {
@@ -10,37 +11,117 @@
         Console.WriteLine("4 - Работа с файлами");
     }
 
-    static Int32 EnterNumber()
+    static double EnterNumber()
     {
-        Int32 num;
-        try
-        {
-            num = Convert.ToInt32(Console.ReadLine());
-        }
-        catch
+        double result;
+        while (!Double.TryParse(Console.ReadLine(), out result))
         {
             Console.WriteLine("Некорректные входные данные!");
-            num = EnterNumber();
         }
-        return num;
+        return result;
+
+    }
+    static void ConvertTemperature()
+    {
+        char from, to;
+        Console.WriteLine("Введите значение температуры:");
+        double num = EnterNumber();
+        Console.WriteLine("Введите название шкалы (C, K, F):");
+
+        while (!Char.TryParse(Console.ReadLine(), out from)
+                || from != 'C' && from != 'K' && from != 'F')
+        {
+            Console.WriteLine("Некорректные входные данные!");
+        }
+        Console.WriteLine("Введите название шкалы, в которую надо перевести (C, K, F):");
+        while (!Char.TryParse(Console.ReadLine(), out to)
+                || to != 'C' && to != 'K' && to != 'F')
+        {
+            Console.WriteLine("Некорректные входные данные!");
+        }
+        if (from == 'C')
+        {
+            if (to == 'K')
+            {
+                num += 273.15;
+            } else if (to == 'F')
+            {
+                num = num * 1.8 + 32;
+            }
+        } else if (from == 'K')
+        {
+            if (to == 'C')
+            {
+                num -= 273.15;
+            }
+            else if (to == 'F')
+            {
+                num = (num - 273.15) * 1.8 + 32;
+            }
+        } else
+        {
+            if (to == 'C')
+            {
+                num = (num - 32) / 1.8;
+            }
+            else if (to == 'K')
+            {
+                num = (num - 32) / 1.8 + 273.15;
+            }
+        }
+        Console.WriteLine($"Результат: {num}");
     }
 
-    static void Run()
+    static void Palindrom()
     {
-        Int32 num = EnterNumber();
-        switch (num)
+        Console.Write("Введите слово: ");
+        string? word = Console.ReadLine();
+        if (word?.Length == 0)
+        {
+            Console.WriteLine("Введена пустая строка, нельзя определить палиндром ли это!");
+            return;
+        }
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (word[i] != word[word.Length - i - 1])
+            {
+                Console.WriteLine("Это не палиндром!");
+                return;
+            }
+        }
+        Console.WriteLine("Это палиндром!");
+    } 
+    static void Rabbit()
+    {
+        Console.WriteLine("Введите число месяцев:");
+        int countMounths = (int)EnterNumber();
+        int count = 2;
+        int diff = 2;
+        for (int i = 1; i < countMounths; i++)
+        {
+            count += diff;
+            diff = count - diff;
+        }
+        Console.WriteLine($"Count of rabbits = {count}");
+    }
+
+    static void 
+
+    static bool Run()
+    {
+        switch (EnterNumber())
         {
             case 0:
                 Console.WriteLine("Пока!");
-                return;
+                return false;
             case 1:
-                Console.WriteLine("Перевод градусов!");
+                ConvertTemperature();
                 break;
             case 2:
-                Console.WriteLine("Определение палиндрома!");
+                Palindrom();
                 break;
             case 3:
-                Console.WriteLine("Задача о размножении кроликов!");
+                Rabbit();
                 break;
             case 4:
                 Console.WriteLine("Работа с файлами!");
@@ -50,10 +131,16 @@
                 Run();
                 break;
         }
+        return true;
     }
     static void Main()
     {
-        PrintMenu();
-        Run();
+        bool isContinued = true;
+        while (isContinued)
+        {
+            PrintMenu();
+            isContinued = Run();
+        }
+
     }
-}  
+}
